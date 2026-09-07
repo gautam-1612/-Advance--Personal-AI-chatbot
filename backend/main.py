45f50fc7de11383847b84815eab585f6bf778067
Gautam Bhushan <gautambhushan5@gmail.com>
2026-09-07 17:43:18 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://advance-personal-ai-chatbot-f336.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
